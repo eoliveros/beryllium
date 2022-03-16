dba8f809e2dbd4c1cc00452586c644b56f0c96c6
eoliveros <47799652+eoliveros@users.noreply.github.com>
2022-03-16 15:18:36 +1300

    Revert "Alex changes"

--- bitcoin-cli.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec bitcoind bitcoin-cli -datadir="/data" "$@"
--- build-docker-images.sh
+++ build-docker-images.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-docker build -t clightning_bitcoin clightning_bitcoin
 docker build -t beryllium_app .
